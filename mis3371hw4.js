2f3f04c9f6f6f82431c2da6dfdd3fb777685748a
Faduran38 <faduran@cougarnet.uh.edu>
2026-05-05 20:23:25 -0500

    Update mis3371hw4.js

--- mis3371hw4.js
+++ mis3371hw4.js
@@ -1,1 +1,8 @@
+/*
+Author: Faviana A Duran 
+Program Name: mis3371hw4.js
+Date Created: April 30, 2026
+Date Updated: May 8, 2026
+Description: Validating elements and adding advanced editing 
+*/
 
